81e4739a06fd6ea7ce3ee4a68444d221140d8d6c
Adrian Camacho <adriancamacho18@gmail.com>
2021-11-12 14:13:36 -0800

    add prop to single card and useeffect logic to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SingleCard from './components/SingleCard'
 import './App.css';
 
 const cardImages = [
-  {'src': '/img/helmet-1.png'},
-  {'src': '/img/potion-1.png'},
-  {'src': '/img/ring-1.png'},
-  {'src': '/img/scroll-1.png'},
-  {'src': '/img/shield-1.png'},
-  {'src': '/img/sword-1.png'}
+  {'src': '/img/helmet-1.png', matched: false},
+  {'src': '/img/potion-1.png', matched: false},
+  {'src': '/img/ring-1.png', matched: false},
+  {'src': '/img/scroll-1.png', matched: false},
+  {'src': '/img/shield-1.png', matched: false},
+  {'src': '/img/sword-1.png', matched: false}
 ]
 
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   const shuffleCards = () => {
     const shuffleCards = [...cardImages, ...cardImages]
@@ -24,7 +26,37 @@ function App() {
       setTurns(0)
   }
 
-  console.log(cards, turns)
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className="App">
@@ -32,7 +64,11 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map(card => (
-          <SingleCard key={card.id} card={card}/>
+          <SingleCard 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched} />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './SingleCard.css'
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice, flipped }) => {
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
   return (
     <div className='card'>
-      <div>
+      <div className={flipped ? 'flipped' : ''}>
         <img className='front' src={card.src} alt='card front'/>
-        <img className='back' src="/img/cover.png" alt="card back"/>
+        <img 
+          onClick={handleClick}
+          className='back' 
+          src="/img/cover.png" 
+          alt="card back"/>
       </div>
     </div>
   )
