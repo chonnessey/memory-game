90361ef857798a285fb88e25bf0ef7ccc62f2b0b
Adrian Camacho <adriancamacho18@gmail.com>
2021-11-12 11:32:57 -0800

    add initial styling for grid

--- src/App.js
+++ src/App.js
@@ -29,6 +29,16 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt='card front'/>
+              <img className='back' src="/img/cover.png" alt="card back"/>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
